f901b0e96dc190c55f30354be16035c2fc003f2b
김태우 <taewoo@Taewoo.local>
2021-01-21 01:04:38 +0900

    머신러닝Flask clustering module

--- 04.Machine_Learning/bp9_cluster/cluster.py
+++ 04.Machine_Learning/bp9_cluster/cluster.py
@@ -8,7 +8,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt 
 from my_util.weather import get_weather
-
+from my_util.clustering import clustering
 cluster_bp = Blueprint('cluster_bp', __name__)
 
 def get_weather_main():
@@ -37,45 +37,6 @@ def cluster():
         current_app.logger.debug(f"{k_number}, {f_csv}, {file_csv}")
 
         df_csv = pd.read_csv(file_csv)
-        # 전처리 - 정규화
-        scaler = StandardScaler()
-        X_scaled = scaler.fit_transform(df_csv.iloc[:, :-1])
-
-        # 차원 축소(PCA)
-        pca = PCA(n_components=2)
-        pca_array = pca.fit_transform(X_scaled)
-        df = pd.DataFrame(pca_array, columns=['pca_x', 'pca_y'])
-        df['target'] = df_csv.iloc[:, -1].values
-
-        # K-Means Clustering
-        kmeans = KMeans(n_clusters=k_number, init='k-means++', max_iter=300, random_state=2021)
-        kmeans.fit(X_scaled)
-        df['cluster'] = kmeans.labels_
-
-        # 시각화
-        markers = ['s', 'o', '^', 'P', 'D', 'H', 'x']
-        plt.figure()
-        for i in df.target.unique():
-            marker = markers[i]
-            x_axis_data = df[df.target == i]['pca_x']
-            y_axis_data = df[df.target == i]['pca_y']
-            plt.scatter(x_axis_data, y_axis_data, marker=marker)
-        plt.title('Original Data Visualization by 2 PCA Components')
-        plt.xlabel('PCA 1'); plt.ylabel('PCA 2')
-        img_file = os.path.join(current_app.root_path, 'static/img/cluster0.png')
-        plt.savefig(img_file)
-
-        plt.figure()
-        for i in range(k_number):
-            marker = markers[i]
-            x_axis_data = df[df.cluster == i]['pca_x']
-            y_axis_data = df[df.cluster == i]['pca_y']
-            plt.scatter(x_axis_data, y_axis_data, marker=marker)
-        plt.xlabel('PCA 1'); plt.ylabel('PCA 2')
-        plt.title(f'{k_number} Clusters Visualization by 2 PCA Components')
-        img_file = os.path.join(current_app.root_path, 'static/img/cluster1.png')
-        plt.savefig(img_file)
-
-        mtime = int(os.stat(img_file).st_mtime)
+        mtime = clustering(df_csv, k_number)
         return render_template('cluster/cluster_res.html', menu=menu, weather=get_weather_main(),
                                 k_number=k_number, mtime=mtime)
